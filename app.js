6a830164260419793e5eeff6b152da6c377df416
vova <wenom998@gmail.com>
2019-10-21 17:29:51 +0300

    ports ProcFile to Heroku added v2

--- app.js
+++ app.js
@@ -28,8 +28,8 @@ app.use(express.static("public"));
 
 const uri = "mongodb+srv://vova:"+ process.env.BD_PASS +"@bloglvluptest-iyvhk.mongodb.net/blog_base?retryWrites=true&w=majority";
 mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {console.log("DB Connected")})
-  .catch(console.log());
+  .then(() => console.log('MongoDB connected...'))
+  .catch(err => console.log(err));
 
 mongoose.set("useCreateIndex", true);
 
